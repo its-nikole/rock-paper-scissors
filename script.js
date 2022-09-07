a88c2535621c435df214d4cf068434fb360fb5cf
heeyniknik <nikole.official@outlook.com>
2022-09-07 21:12:00 +0300

    feat: :zap: Increment Score
    Your and Computer Score Increments

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const paperBtn = document.getElementById('paper')
 const scissorsBtn = document.getElementById('scissors')
 const yourEmoji = document.querySelector('.your-output')
 const computerEmoji = document.querySelector('.computer-output')
+const yourScore = document.getElementById('yourScore')
+const computerScore = document.getElementById('computerScore')
+
 
 function getComputerChoice() {
     const computerChoises = choises[Math.floor(Math.random() * choises.length)]
@@ -14,21 +17,32 @@ function checkForWinner(playerSelection) {
     const computerChoise = getComputerChoice()
     
     if(playerSelection === computerChoise) {
-        console.log("Tie")
+      console.log("Tie")
     } else if (playerSelection === choises[0] && computerChoise === choises[2]) {
-        console.log("Win")
+      console.log("Win")
+      incrementScore(yourScore)
     } else if (playerSelection === choises[1] && computerChoise === choises[0]) {
         console.log("Win")
+        incrementScore(yourScore)
     } else if (playerSelection === choises[2] && computerChoise === choises[1]) {
         console.log("Win") 
+        incrementScore(yourScore)
     } else if (playerSelection === choises[0] && computerChoise === choises[1]) {
         console.log("Lose") 
+        incrementScore(computerScore)
     } else if (playerSelection === choises[1] && computerChoise === choises[2]) {
         console.log("Lose") 
+        incrementScore(computerScore)
     } else if (playerSelection === choises[2] && computerChoise === choises[0]) {
         console.log("Lose") 
+        incrementScore(computerScore)
     }   
 }
+
+function incrementScore(newScore) {
+  newScore.innerHTML = Number(newScore.innerText) +1
+} 
+
 
 function addYourEmoji(playerSelection) {
   if(playerSelection === choises[0]) {
