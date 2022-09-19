7c908a33fd877349448ecc7118a2bf2f85a42581
heeyniknik <nikole.official@outlook.com>
2022-09-19 23:10:57 +0300

    Ongoing

--- script.js
+++ script.js
@@ -1,89 +1,124 @@
-const choises = ['rock', 'paper', 'scissors']
-const rockBtn = document.getElementById('rock')
-const paperBtn = document.getElementById('paper')
-const scissorsBtn = document.getElementById('scissors')
-const yourEmoji = document.querySelector('.your-output')
-const computerEmoji = document.querySelector('.computer-output')
-const yourScore = document.getElementById('yourScore')
-const computerScore = document.getElementById('computerScore')
+const choises = ["rock", "paper", "scissors"];
+const totalRounds = 5;
+let currentRound = 0;
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const playerEmoji = document.querySelector(".your-output");
+const computerEmoji = document.querySelector(".computer-output");
+const yourScore = document.getElementById("yourScore");
+const computerScore = document.getElementById("computerScore");
+const newRoundBtn = document.querySelector('.new-round-button');
 
+ 
 
 function getComputerChoice() {
-    const computerChoises = choises[Math.floor(Math.random() * choises.length)]
-    return computerChoises
-}
-  
-function checkForWinner(playerSelection) {
-    const computerChoise = getComputerChoice()
-    
-    if(playerSelection === computerChoise) {
-      console.log("Tie")
-      incrementScore(yourScore), incrementScore(computerScore)
-    } else if (playerSelection === choises[0] && computerChoise === choises[2]) {
-      console.log("Win")
-      incrementScore(yourScore)
-    } else if (playerSelection === choises[1] && computerChoise === choises[0]) {
-        console.log("Win")
-        incrementScore(yourScore)
-    } else if (playerSelection === choises[2] && computerChoise === choises[1]) {
-        console.log("Win") 
-        incrementScore(yourScore)
-    } else if (playerSelection === choises[0] && computerChoise === choises[1]) {
-        console.log("Lose") 
-        incrementScore(computerScore)
-    } else if (playerSelection === choises[1] && computerChoise === choises[2]) {
-        console.log("Lose") 
-        incrementScore(computerScore)
-    } else if (playerSelection === choises[2] && computerChoise === choises[0]) {
-        console.log("Lose") 
-        incrementScore(computerScore)
-    }   
+  const computerChoises = choises[Math.floor(Math.random() * choises.length)];
+  return computerChoises;
 }
 
-function incrementScore(newScore) {
-  newScore.innerHTML = Number(newScore.innerText) +1
-} 
-
-function emojiDiv(){
-  document.createElement
-}
-
-function addYourEmoji(playerSelection) {
-  if(playerSelection === choises[0]) {
-    yourEmoji.innerHTML += "✊"
-  } else if (playerSelection === choises[1]) {
-    yourEmoji.innerHTML += "✋"
-  } else if (playerSelection === choises[2]) {
-    yourEmoji.innerHTML += "✌"
-  } 
-}
-
-function addComputerEmoji(computerChoise) {
-  if (computerChoise === choises[0]) {
-    computerEmoji.innerHTML += "✊"
-  } else if (computerChoise === choises[1]) {
-    computerEmoji.innerHTML += "✋"
-  } else if (computerChoise === choises[2]) {
-    computerEmoji.innerHTML += "✌"
+function checkForWinner(playerSelection, computerChoise) {
+  if (playerSelection === computerChoise) {
+    return "Tie";
+  } else if (playerSelection === choises[0] && computerChoise === choises[2]) {
+    return "Win";
+  } else if (playerSelection === choises[1] && computerChoise === choises[0]) {
+    return "Win";
+  } else if (playerSelection === choises[2] && computerChoise === choises[1]) {
+    return "Win";
+  } else if (playerSelection === choises[0] && computerChoise === choises[1]) {
+    return "Lose";
+  } else if (playerSelection === choises[1] && computerChoise === choises[2]) {
+    return "Lose";
+  } else if (playerSelection === choises[2] && computerChoise === choises[0]) {
+    return "Lose";
   }
 }
 
-rockBtn.addEventListener('click', (e) => {
-    checkForWinner(e.target.id)
-    addYourEmoji(e.target.id)
-    addComputerEmoji(getComputerChoice())
+function incrementScore(newScore) {
+  newScore.innerHTML = Number(newScore.innerText) + 1;
+}
+
+function emojiDiv() {
+  document.createElement;
+}
+const drawEmoji = (element, emoji) => {
+  element.innerHTML += emoji;
+};
+
+const getEmoji = (choice) => {
+  if (choice === choises[0]) return "✊";
+  if (choice === choises[1]) return "✋";
+  if (choice === choises[2]) return "✌";
+};
+
+const addEmoji = (choise, type) => {
+  type === "player"
+    ? drawEmoji(playerEmoji, getEmoji(choise))
+    : drawEmoji(computerEmoji, getEmoji(choise));
+};
+
+const whoGetsScore = (outcome) => {
+  if (outcome === "Tie") {
+    
+    incrementScore(yourScore);
+    incrementScore(computerScore);
+  }
+  if (outcome === "Win") incrementScore(yourScore);
+  if (outcome === "Lose") incrementScore(computerScore);
+};
+
+const checkIfGameOver = () => {
+  currentRound +=1
+
+  if(currentRound === totalRounds){
+    const playerPrintedScore = yourScore.innerText
+    const computerPrintedScore = computerScore.innerText
+    gameOverState()
+    if(playerPrintedScore === computerPrintedScore){
+      console.log("Tie")
+      return
+    }
+    playerPrintedScore> computerPrintedScore ? console.log("You win") : console.log("Computer wins")
+  }
+
+}
+const gameOverState = () => {
+  rockBtn.setAttribute('disabled', true)
+  paperBtn.setAttribute('disabled',true)
+  scissorsBtn.setAttribute('disabled',true)
+  newRoundBtn.classList.remove('hidden')
+
+}
+
+
+
+
+const clickHandler = (event) => {
+  const generatedComputerChoice = getComputerChoice();
+  const playerChoice = event.target.id;
+  const outcome = checkForWinner(playerChoice, generatedComputerChoice);
+  whoGetsScore(outcome);
+  addEmoji(playerChoice, "player");
+  addEmoji(generatedComputerChoice, "computer");
+  checkIfGameOver()
+
+};
+
+rockBtn.addEventListener("click", clickHandler);
+
+paperBtn.addEventListener("click", clickHandler);
+
+scissorsBtn.addEventListener("click", clickHandler);
+
+newRoundBtn.addEventListener('click', function(){
+  newRoundBtn.classList.add('hidden')
+  rockBtn.removeAttribute('disabled')
+  paperBtn.removeAttribute('disabled')
+  scissorsBtn.removeAttribute('disabled')
+  yourScore.innerText = 0
+  computerScore.innerText = 0
+  playerEmoji.innerHTML = ''
+  computerEmoji.innerHTML=''
+  currentRound = 0
 })
-
-
-paperBtn.addEventListener('click', (e) => {
-    checkForWinner(e.target.id)
-    addYourEmoji(e.target.id)
-    addComputerEmoji(getComputerChoice())
-})
-
-scissorsBtn.addEventListener('click', (e) => {
-    checkForWinner(e.target.id)
-    addYourEmoji(e.target.id)
-    addComputerEmoji(getComputerChoice())
-})
-
